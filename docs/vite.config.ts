5961b2a07ea2836acf8c7209fef29e63b2bb6dfd
claudsaints <claudiodsantoss7@gmail.com>
2025-05-26 19:17:44 -0300

    teste

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/claudsaints",
+  base: "/ra2581392413005",
  
 })
