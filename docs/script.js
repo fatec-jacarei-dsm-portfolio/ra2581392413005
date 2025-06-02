0db522b5e7b00b293b0a37664a8877694b9afe24
claudsaints <claudiodsantoss7@gmail.com>
2025-06-02 20:20:06 -0300

    feat: (contatos)

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,26 @@
-const data = [
+const academicData = [
   {
     id: "1",
     image: "./assets/logo_labw.png",
     title: "LABWIND",
-    description: "Aplicação com foco em solucões metereológicas para lago de furnas",
+    description: "Aplicação com foco em solucões metereológicas para lago de furnas.",
+    part: "Atuei como desenvolvedor.",
     link: "https://github.com/Steel-Hard/LabWind"
   },
   {
     id: "2",
     image: "./assets/logo_vita.png",
     title: "VITAL-U",
-    description: "Aplicação para gerenciamento de calorias",
+    description: "Aplicação para gerenciamento de calorias.",
+    part: "Atuei como Scrum Master.",
     link: "https://github.com/Steel-Hard/VitalU"
   },
   {
     id: "3",
     image: "./assets/logo_hast.png",
     title: "HARDSTOP",
-    description: "Ambiente de apredizagem virtual sobre SCRUM",
+    description: "Ambiente de apredizagem virtual sobre SCRUM.",
+    part: "Atuei como desenvolvedor.",
     link: "https://github.com/frevisto/hardstop"
   }
 ];
@@ -28,7 +31,7 @@ document.getElementById("time-display").textContent =
 
 const container = document.getElementById("projects");
 
-data.forEach((project) => {
+academicData.forEach((project) => {
   const div = document.createElement("div");
   div.className = "project";
   div.innerHTML = `
@@ -36,7 +39,10 @@ data.forEach((project) => {
     <img  src="${project.image}" alt="${project.title}" />
   </a>
     <h2>${project.title}</h2>
-    <p>${project.description}</p>
+    <div>    
+        <p> <span>Sobre:        </span> ${project.description}  </p>
+        <p> <span>Participação: </span> ${project.part}         </p>
+    <div/>
   `;
   container.appendChild(div);
 });
